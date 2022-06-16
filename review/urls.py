77ce6ca820bb21aeefe28d6d77ffee3be9b8160e
martin023 <mainamartin023@gmail.com>
2022-06-16 16:27:52 +0300

    THis commit will update the diffrent routes

--- review/urls.py
+++ review/urls.py
@@ -2,7 +2,14 @@ from django.urls import path
 from . import views
 urlpatterns = [
 
-    path('',views.home, name='home' ),
-    path('register/',views.register, name='register' ),
-    path('postproject/',views.post_proj, name='post' ),
+    path('', views.home, name='homepage' ),
+    path('endpoints/api', views.apipage, name='api-details' ),
+    path('sites/new/', views.NewSite, name='new-site'),
+    path('rate/', views.rate_site, name='rate-site'),
+    path('rateusability/', views.rate_usability, name='rate-usability'),
+    path('ratecontent/', views.rate_content, name='rate-content'),
+    path('sites/<int:pk>', views.View_site, name='view-site'),
+    path('search/', views.search_results, name='search_results'),
 ]
+
+
