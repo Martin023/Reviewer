31de0c5bf38ea114bb449549449bba2cef47808a
martin023 <mainamartin023@gmail.com>
2022-06-16 16:12:25 +0300

    load imports to settings

--- reviewer/settings.py
+++ reviewer/settings.py
@@ -12,6 +12,13 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 import cloudinary
 from pathlib import Path
+import cloudinary.uploader
+import cloudinary.api
+from pathlib import Path
+import django_heroku
+import dj_database_url
+from decouple import config,Csv
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
