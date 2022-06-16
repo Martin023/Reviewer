80dd807df63f9bf4e589b0c42e0e0a158c9d4b17
martin023 <mainamartin023@gmail.com>
2022-06-16 16:40:20 +0300

    This commit will add saving profile function

--- users/models.py
+++ users/models.py
@@ -4,3 +4,34 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 # Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = CloudinaryField('image')
+    display_name = models.CharField(max_length=100, null=True, blank=True)
+    url = models.URLField(max_length=200, null=True, blank=True)
+    who = models.CharField(max_length=100, null=True, blank=True)
+    country = models.CharField(max_length=100, null=True, blank=True)
+    twitter = models.CharField(max_length=100, null=True, blank=True)
+    facebook = models.CharField(max_length=100, null=True, blank=True)
+    pinterest = models.CharField(max_length=100, null=True, blank=True)
+    linkedin = models.CharField(max_length=100, null=True, blank=True)
+    
+    @property
+    def username(self):
+        return self.user.username
+    def __str__(self):
+        return f'{self.user.username} Profile'
+    
+    @receiver(post_save, sender=User)
+    def create_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+            
+
+    @receiver(post_save, sender=User)
+    def save_profile(sender, instance, **kwargs):
+        instance.profile.save()
+    
+  
