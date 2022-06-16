b1e9da883fc3268d372b833fe314792b0f61b486
martin023 <mainamartin023@gmail.com>
2022-06-16 16:25:02 +0300

    This commit will import cloudinary fields to forms.py for review

--- review/forms.py
+++ review/forms.py
@@ -3,7 +3,23 @@ from .models import WebReview
 from cloudinary.forms import CloudinaryFileField
 
 
-class WebReviewForm(forms.ModelForm):
+CATEGORIES_1 = [
+    ('Ecommerce', 'Ecommerce'),
+    ('Portfolio', 'Portfolio'),
+    ('DjangoApp', 'DjangoApp'),
+    ('ArtsnCulture', 'ArtsnCulture'),
+]
+
+class NewSiteForm(forms.ModelForm):
+    
+    sitename =forms.CharField(widget=forms.TextInput(attrs={'class': 'Input', 'placeholder': 'Image Caption'}), required=True)
+    url =forms.URLField( required=True)
+    description = forms.CharField(widget=forms.TextInput(attrs={'class': 'Input', 'placeholder': 'Project description '}), required=True)
+    category =forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple(attrs={'class': 'Input', 'placeholder': 'Choose a category'}),choices=CATEGORIES_1, required=True)
+    country = forms.CharField(widget=forms.TextInput(attrs={'class': 'Input', 'placeholder': 'Country '}), required=True)
+    technologies = forms.CharField(widget=forms.TextInput(attrs={'class': 'Input', 'placeholder': 'Technologies used | separate with comma '}), required=True)
+    image = CloudinaryFileField()
+    
     class Meta:
-        model = WebReview
-        fields = ('name','description','screenshot')
+        model = Site
+        fields = ['sitename', 'url', 'description', 'category', 'country', 'technologies', 'image']
