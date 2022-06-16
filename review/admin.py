521d083ee39c4ce70a87c88c101a5f56f9ff5497
martin023 <mainamartin023@gmail.com>
2022-06-16 16:22:22 +0300

    This commit will register review models to the admin panel

--- review/admin.py
+++ review/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(WebReview),
-admin.site.register(Rating),
-admin.site.register(Category)
+admin.site.register(Technologies)
+admin.site.register(Site)
+admin.site.register(Rating)
+admin.site.register(RatingUsability)
+admin.site.register(RatingContent)
